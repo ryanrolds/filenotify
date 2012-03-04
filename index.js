53140b57aef66fe7de17f022daaaa5b6c83bac4c
Ryan Olds <ryanrolds@gmail.com>
2012-03-03 20:49:17 -0600

    Initial

---
+++ index.js
@@ -0,0 +1,31 @@
+
+var events = require('events');
+var util = require('util');
+var fs = require('fs');
+
+module.exports = function() {
+  var FileChanges = function FileChanges(watchable) {
+    // If string treat as file, else check if readable stream
+    if(typeof watchable === 'string') {
+      //events.EventEmitter.call(this);
+      var caller = this;
+      fs.watchFile(watchable, {'persistent': true}, function(curr, prev) {
+        if(curr.mtime !== prev.mtime) {
+          var size = curr.size - prev.size;
+          var buffer = new Buffer(size);
+
+          fs.open(watchable, 'r', function(error, fd) {
+            fs.read(fd, buffer, 0, size, prev.size, function(error, bytes, buff) {
+              caller.emit('data', buff.toString());
+            });
+          });
+        }
+      });
+    } else {
+      throw new Error('Invalid watchable');
+    }
+  }
+
+  util.inherits(FileChanges, events.EventEmitter);
+  return FileChanges;
+}();
---
+++ tests/watcher.test.js
@@ -0,0 +1,37 @@
+
+require('should');
+
+var fs = require('fs');
+
+var Watcher = require('../');
+
+describe('slogger', function() {
+  describe('file watcher', function() {
+    var watcher = new Watcher(__dirname + '/assets/access.log');
+
+    it('is an instance of Watcher', function() {
+      watcher.should.be.instanceof(Watcher);
+    });
+
+    it('emits data on change', function(done) {
+      watcher.on('data', function(data) {
+        data.should.equal('blah\n');
+        done();
+      });
+
+      var file = fs.createWriteStream(__dirname + '/assets/access.log', {'flags': 'a'});
+      file.end('blah\n', 'utf8');
+    });
+
+    it('should error with Invalid watchable', function() {
+      try {
+        var watcher = new Watcher();
+      } catch(e) {
+        e.message.should.equal('Invalid watchable');
+      }
+    });
+
+    // @TODO make sure it chandle overwritting the file
+    // @TODO improve permission issue handling
+  });
+});
